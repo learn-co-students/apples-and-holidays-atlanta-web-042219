4e1dd3b1cf1675b9251a2962c23068d394684475
Dhara Patel <1991dharapatel@gmail.com>
2019-03-24 16:36:08 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,12 +18,17 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
+  
+  holiday_hash[:winter].collect do |x, y|
+    y << supply
+  end
 
 end
 
@@ -31,18 +36,27 @@ end
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  
+  holiday_hash[:spring].collect do |x, y|
+    y << supply
+  end
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
+  
+  holiday_hash[season][holiday_name] = supply_array
+  
+  holiday_hash
 
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  # binding.pry
+  holiday_hash[:winter].values.flatten
+  
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,13 +67,40 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  # binding.pry
+  
+  
+for name in holiday_hash.keys
+  puts "#{name.to_s.capitalize!}:"
+
+    for inner_name in holiday_hash[name].keys
+  
+    result = []
+    for word in inner_name.to_s.split("_")
+        result << word.capitalize!
+      end
+ 
+        
+    puts "  #{result.join(" ")}: #{holiday_hash[name][inner_name].join(", ")}"
+      
+   end
+ end
 
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  result =[]
+  for inner_name in holiday_hash.keys
+    for name in holiday_hash[inner_name].keys
+    # binding.pry
+    if holiday_hash[inner_name][name].include?("BBQ")
+      result << name
+    end
+    end
+  end
+    result 
 end
 
 
