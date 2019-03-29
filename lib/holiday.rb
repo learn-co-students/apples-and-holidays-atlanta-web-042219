9c8682597b2efc33bef5c8650e9dad24b01ef309
active-object-6703 <nerajno@gmail.com>
2019-03-29 12:58:25 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -45,7 +45,10 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  rtnd_winter_supplies = []
+  winter_supplies = holiday_hash[:winter].values
+  rtnd_winter_supplies.push(winter_supplies)
+  rtnd_winter_supplies
 end
 
 def all_supplies_in_holidays(holiday_hash)
