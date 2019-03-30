3c9e2b07278c62d08930be1edeef2f1b2a1dce22
active-object-6703 <nerajno@gmail.com>
2019-03-30 13:20:33 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,8 +61,8 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash.to_s
-  binding.pry
+  less_tess = holiday_hash.to_s
+  # binding.pry
 end
 
 def all_holidays_with_bbq(holiday_hash)
