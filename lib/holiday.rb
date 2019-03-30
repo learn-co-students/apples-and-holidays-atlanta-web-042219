dac0c51bfe04e5cf68b131dc9057fcf4f0511cae
active-object-6703 <nerajno@gmail.com>
2019-03-30 13:13:54 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,8 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.to_s
+  binding.pry
 end
 
 def all_holidays_with_bbq(holiday_hash)
