fa214914d1f07ea4b4791e8211571ed4eb94d1ea
han xu <hanxu27@gmail.com>
2019-04-11 18:53:57 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,8 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, data| 
     puts "#{season.capitalize}:"
     data.each do |day, supply|
-      output = "  #{day.capitalize}: #{supply.join(", ")}"
-      puts output.gsub("_", " ").titleize
+      puts "  #{day.gsub("_", " ").titleize}: #{supply.join(", ")}"
     end
   end
 end
