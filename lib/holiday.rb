2937e6b7f575edb04aff590e8a36858f09d4acf4
han xu <hanxu27@gmail.com>
2019-04-11 17:14:23 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,6 +18,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  binding.pry
   holiday_hash[:summer][:fourth_of_july][2]
 end
 
