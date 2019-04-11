86272b40f9b9fef0700ab41ac185748e06f8b22d
han xu <hanxu27@gmail.com>
2019-04-11 19:10:23 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,7 +75,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, data|
     data.each do |day, supply|
       supply.each do |item|
-        
+        puts "#{day}"
 end
 
 
