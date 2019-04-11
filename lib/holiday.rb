213ec8cd87a2223b1e29aa1dac2b0312bf456714
han xu <hanxu27@gmail.com>
2019-04-11 17:51:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -35,8 +35,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
   holiday_hash[:winter].each do |days|
     days << "Balloons"
   end
-  binding.pry
-  holiday_hash
+
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
