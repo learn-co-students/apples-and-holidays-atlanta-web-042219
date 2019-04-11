192608933c614ba2418a302e19c9b8d2771c61a3
han xu <hanxu27@gmail.com>
2019-04-11 19:11:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,6 +72,9 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  
+  output = []
+  
   holiday_hash.each do |season, data|
     data.each do |day, supply|
       supply.each do |item|
